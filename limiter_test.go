900d65d4acad4ba37ed3e83268b9b73643ca9a8b
Charles Neau <charles63500@gmail.com>
2018-09-17 15:04:03 +0100

    explained test

--- limiter_test.go
+++ limiter_test.go
@@ -1,17 +1,32 @@
 package limiter
 
 import (
+	"log"
 	"testing"
 	"time"
 )
 
-// TestExample should take 4 seconds
+// TestExample should take a bit more than 400ms (less than 440ms too)
 func TestTime(t *testing.T) {
-	limit := New(4)
-	for i := 0; i < 16; i++ {
+	// params
+	nbJobs := 16
+	nbConc := 4
+	jobTime := time.Millisecond * 100
+
+	start := time.Now()
+	limit := New(nbConc)
+	for i := 0; i < nbJobs; i++ {
 		limit.Execute(func() {
-			time.Sleep(time.Second)
+			time.Sleep(jobTime)
 		})
 	}
 	limit.Wait()
+	duration := time.Now().Sub(start)
+	expected := time.Duration(jobTime) * time.Duration(nbJobs) / time.Duration(nbConc)
+	expectedTenPercent := time.Duration(float64(expected) * 1.1)
+	if duration > expected && duration < expectedTenPercent {
+		log.Println(expectedTenPercent, " > ", duration, " > ", expected)
+	} else {
+		t.FailNow()
+	}
 }
